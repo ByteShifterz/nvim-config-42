d8d3476c165e4286b4308a6581df9d20f10cd749
ByteShifterz <byteshifter@hotmail.com>
2024-10-25 07:01:14 +0200

    En mode siu

--- starter/lua/mappings.lua
+++ starter/lua/mappings.lua
@@ -7,22 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- Remove the Tab and Shift-Tab binds
-local cmp = require "cmp"
-
-local custom_mappings = {
-  ["<Tab>"] = function(fallback)
-    fallback()
-  end,
-  ["<S-Tab>"] = function(fallback)
-    fallback()
-  end,
-}
-
-cmp.setup({
-  mapping = cmp.mapping.preset.insert(custom_mappings),
-})
-
 map('t', '<ESC>', [[<C-\><C-n>]], { noremap = true, silent = true })
 map('n', '<leader><Tab>', '<C-w>w')
 map('n', '<leader><S-Tab>', '<C-w>W')
+map('i', '<C-ESC>', '<ESC>i<Right>', { noremap = true, silent = true })
