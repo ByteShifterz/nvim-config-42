0e5a9d6b2f326b66d5fdb5dfab268bb3a44d4059
ByteShifterz <byteshifter@hotmail.com>
2024-11-07 02:04:12 +0100

    aa

--- starter/lua/configs/lspconfig.lua
+++ starter/lua/configs/lspconfig.lua
@@ -1,4 +1,3 @@
--- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
@@ -6,18 +5,27 @@ local lspconfig = require "lspconfig"
 local servers = { "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
+-- Customize clangd command to specify C language mode
+local clangd_cmd = {
+  "clangd",
+  "--header-insertion=never", -- Prevents automatic header insertion
+  "--compile-commands-dir=.", -- Adjust as needed for your project
+}
+
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+  if lsp == "clangd" then
+    lspconfig.clangd.setup {
+      cmd = clangd_cmd,
+      on_attach = nvlsp.on_attach,
+      on_init = nvlsp.on_init,
+      capabilities = nvlsp.capabilities,
+    }
+  else
+    lspconfig[lsp].setup {
+      on_attach = nvlsp.on_attach,
+      on_init = nvlsp.on_init,
+      capabilities = nvlsp.capabilities,
+    }
+  end
 end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
