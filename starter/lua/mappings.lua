8bfdd4ca0ebce17c9f0cdd03257946e95e94645b
ByteShifterz <byteshifter@hotmail.com>
2024-11-07 02:10:37 +0100

    fix luasnip tab

--- starter/lua/mappings.lua
+++ starter/lua/mappings.lua
@@ -15,13 +15,17 @@ map('i', '<C-ESC>', '<ESC>i<Right>', { noremap = true, silent = true })
 -- Remove the Tab and Shift-Tab binds
 local cmp = require "cmp"
 local custom_mappings = {
-  ["<Tab>"] = function(fallback)
-    fallback()
-  end,
-  ["<S-Tab>"] = function(fallback)
-    fallback()
-  end,
+	["<Tab>"] = cmp.mapping(function(fallback)
+		if require("luasnip").expand_or_jumpable() then
+			require("luasnip").expand_or_jump()
+		else
+			fallback()
+		end
+	end, { "i", "s" }),
+	["<S-Tab>"] = function(fallback)
+		fallback()
+	end,
 }
 cmp.setup({
-  mapping = cmp.mapping.preset.insert(custom_mappings),
+	mapping = cmp.mapping.preset.insert(custom_mappings),
 })
